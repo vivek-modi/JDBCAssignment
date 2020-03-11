7c8e6b85ca8e678096c0f92dc3473541d10e6c20
Vivek Modi <vivek.modi456@gmail.com>
2020-03-11 19:20:42 +0000

    Database services of insert user

---
+++ DataBaseJDBC/src/com/services/DatabaseService.java
@@ -0,0 +1,26 @@
+package com.services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.database.DbConnect;
+
+public class DatabaseService {
+
+	public static boolean Register_user(String username, String password, int type) {
+		try {
+			PreparedStatement statement = DbConnect.getInstance()
+					.prepareStatement("insert into login(username,password,type) values(?,?,?)");
+			statement.setString(1, username);
+			statement.setString(2, password);
+			statement.setInt(3, type);
+			return (statement.executeUpdate() > 0) ? true : false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+
+}
