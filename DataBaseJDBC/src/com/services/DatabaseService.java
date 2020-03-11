77f0a6929f8b5fa6ab87c590a2eaa3c7ae2d86d9
Vivek Modi <vivek.modi456@gmail.com>
2020-03-11 20:30:47 +0000

    Added login select service

--- DataBaseJDBC/src/com/services/DatabaseService.java
+++ DataBaseJDBC/src/com/services/DatabaseService.java
@@ -20,7 +20,26 @@ public class DatabaseService {
 			e.printStackTrace();
 		}
 		return false;
+	}
 
+	public static int Login(String username, String password) {
+		int num = 2;
+		try {
+			PreparedStatement statement = DbConnect.getInstance()
+					.prepareStatement("SELECT * FROM login where username=? AND password=?");
+			statement.setString(1, username);
+			statement.setString(2, password);
+			ResultSet set = statement.executeQuery();
+			while (set.next()) {
+				if (set.getString(1).equals(username) && set.getString(2).equals(password)) {
+					num = (Integer.parseInt(set.getString(3)) == 1) ? 1 : 0;
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return num;
 	}
 
 }
