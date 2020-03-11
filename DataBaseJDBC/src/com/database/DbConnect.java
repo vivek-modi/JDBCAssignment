17edb4250d1a6758f937aa99b663db8bee4afa88
Vivek Modi <vivek.modi456@gmail.com>
2020-03-11 19:21:05 +0000

    Create single instance of class

--- DataBaseJDBC/src/com/database/DbConnect.java
+++ DataBaseJDBC/src/com/database/DbConnect.java
@@ -1,5 +1,27 @@
 package com.database;
 
-public class DbConnect {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DbConnect {
+	static Connection connection = null;
+
+	public static Connection getConnection() {
+
+		if (connection == null) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/assignment?useSSL=false", "root",
+						"admin");
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
+
+	public static Connection getInstance() {
+		return (connection == null) ? connection = DbConnect.getConnection() : connection;
+	}
 }
