8dd86c3e8a733a671c1b5366cdba95d70bac092b
Vivek Modi <vivek.modi456@gmail.com>
2020-03-11 20:31:04 +0000

    Added Two new Screen

---
+++ DataBaseJDBC/src/com/mainscreen/CustomerScreen.java
@@ -0,0 +1,43 @@
+package com.mainscreen;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class CustomerScreen {
+
+	protected JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CustomerScreen window = new CustomerScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CustomerScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
