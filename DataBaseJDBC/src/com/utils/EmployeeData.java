eaae94a735594c4d1b40e938094ca65b1d0107a6
Vivek Modi <vivek.modi456@gmail.com>
2020-03-13 15:32:25 +0000

    Created Employee Getter and Setter Class

---
+++ DataBaseJDBC/src/com/utils/EmployeeData.java
@@ -0,0 +1,65 @@
+package com.utils;
+
+public class EmployeeData {
+
+	private int id;
+	private String firstName, lastName, gender, designation, department;
+
+	public EmployeeData(int id, String firstName, String lastName, String gender, String designation,
+			String department) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.designation = designation;
+		this.department = department;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+}
