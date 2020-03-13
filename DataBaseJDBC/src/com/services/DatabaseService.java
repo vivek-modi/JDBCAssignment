ba63215906db7e8c93edc3d50931f642633ad1ce
Vivek Modi <vivek.modi456@gmail.com>
2020-03-13 03:33:09 +0000

    Show Product data and add to export file

--- DataBaseJDBC/src/com/services/DatabaseService.java
+++ DataBaseJDBC/src/com/services/DatabaseService.java
@@ -1,12 +1,19 @@
 package com.services;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.swing.table.DefaultTableModel;
+
 import com.database.DbConnect;
+import com.utils.ProductData;
 
 public class DatabaseService {
 
@@ -63,7 +70,6 @@ public class DatabaseService {
 
 	public static boolean InsertItem(String itemName, String itemType, int price, int qunatity) {
 		try {
-			String sql = "";
 			PreparedStatement statement = DbConnect.getInstance().prepareStatement(
 					"insert into items(item_name,item_type,item_price,item_quantity) values(?,?,?,?)");
 			statement.setString(1, itemName);
@@ -77,4 +83,38 @@ public class DatabaseService {
 		return false;
 	}
 
+	public static ArrayList<ProductData> ReadProductData() {
+		ArrayList<ProductData> arrayList = new ArrayList<>();
+		try {
+			PreparedStatement statement = DbConnect.getInstance().prepareStatement("SELECT * FROM items");
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				arrayList.add(
+						new ProductData(rs.getInt(1), rs.getInt(4), rs.getInt(5), rs.getString(2), rs.getString(3)));
+			}
+			return arrayList;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static String ExportProduct() throws IOException {
+		ArrayList<ProductData> data = ReadProductData();
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File("items.csv")));
+
+		if (data == null) {
+			return "No Data Available to Generate";
+		} else {
+			for (ProductData productData : data) {
+				writer.write("Item ID: " + productData.getItem_id() + "\nItem Name: " + productData.getItem_name()
+						+ "\nItem Type: " + productData.getItem_type() + "\nItem Price: " + productData.getItem_price()
+						+ "\nItem Quantity: " + productData.getItem_quantity() + "\n\n");
+
+			}
+			writer.close();
+			return "Import Successfully";
+		}
+
+	}
 }
